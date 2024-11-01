7c9cef01af7204549fd079038c3a3af6ea4aaa67
TasmiyaKhan8028 <155808841+TasmiyaKhan8028@users.noreply.github.com>
2024-11-01 11:48:07 -0800

    error

---
+++ src/app/api/users/route.js
@@ -0,0 +1,30 @@
+import connectDB from "@/lib/db/connectDB";
+import { UserModal } from "@/lib/models/User";
+
+export async function GET(request) {
+
+    await connectDB()
+    console.log('conneted',connectDB());
+    const users = await UserModal.find()
+    return Response.json(users, {status: 200})
+}
+ 
+
+ 
+export async function POST(request) {
+    await connectDB();
+    
+    
+    const obj = await request.json();
+    let newUser = new UserModal(obj)
+    await newUser.save()
+    return Response.json(newUser, {status: 201})
+}
+ 
+export async function PUT(request) {}
+ 
+export async function DELETE(request) {}
+ 
+
+ 
+        
